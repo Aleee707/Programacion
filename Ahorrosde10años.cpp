c0f4eff543ca0dbd5e0845f60ad820180d6f2894
Ale <alet4451@gmail.com>
2025-06-03 11:01:41 -0600

    Ahorrosde10años.cpp

--- Ahorrosde10años.cpp
+++ Ahorrosde10años.cpp
@@ -11,30 +11,21 @@ int main() {
 
     double totalAhorrado = 0.0;
 
-    std::cout << "--- Calculadora de Ahorro Simple ---" << std::endl;
     std::cout << "Años a ahorrar: " << aniosTotales << std::endl;
     std::cout << "Aporte mensual: $" << std::fixed << std::setprecision(2) << aporteMensual << std::endl;
     std::cout << "Interes mensual: " << tasaInteresMensual * 100 << "%" << std::endl;
-    std::cout << "------------------------------------" << std::endl;
-
 
     for (int mes = 1; mes <= totalMeses; ++mes) {
        
         totalAhorrado = totalAhorrado + aporteMensual;
-
-        
         double interesesGanados = totalAhorrado * tasaInteresMensual;
-
-       
         totalAhorrado = totalAhorrado + interesesGanados;
 
     }
 
-  
-    std::cout << "\n--- Resultado Final ---" << std::endl;
+    std::cout << "\nResultado Final" << std::endl;
     std::cout << "Despues de " << aniosTotales << " anios (o " << totalMeses << " meses)," << std::endl;
     std::cout << "¡Habrias ahorrado un total de: $" << std::fixed << std::setprecision(2) << totalAhorrado << "!" << std::endl;
-    std::cout << "-----------------------" << std::endl;
-
+   
     return 0; 
 }
