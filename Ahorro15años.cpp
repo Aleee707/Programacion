4387ac0c3b53b77d94be6a043355ecd444aaf8f7
Ale <alet4451@gmail.com>
2025-06-03 11:00:42 -0600

    Ahorro15años.cpp

--- Ahorro15años.cpp
+++ Ahorro15años.cpp
@@ -1,18 +1,34 @@
-#include <iostream>
-#include <cmath> 
-
-using namespace std;
+#include <iostream> 
+#include <iomanip> 
 
 int main() {
     
-    double pagoMensual = 15000.0;
-    double interesMensual = 0.037; // 3.7% mensual
-    int duracionAnios = 15;
-    int totalPeriodos = duracionAnios * 12;
-    
-    double ahorroTotal = pagoMensual * ((pow(1 + interesMensual, totalPeriodos) - 1) / interesMensual);
-    
-    cout << "El total acumulado tras " << duracionAnios << " anos de ahorro es: $" << ahorroTotal << endl;
+    int aniosTotales = 15;
+    double aporteMensual = 15000.0; 
+    double tasaInteresMensual = 0.037; 
 
-    return 0;
+   
+    int totalMeses = aniosTotales * 12;
+
+    
+    double totalAhorrado = 0.0;
+
+    std::cout << "Anos a ahorrar: " << aniosTotales << std::endl;
+    std::cout << "Aporte mensual: $" << std::fixed << std::setprecision(2) << aporteMensual << std::endl;
+    std::cout << "Interes mensual: " << tasaInteresMensual * 100 << "%" << std::endl;
+    
+    for (int mes = 1; mes <= totalMeses; ++mes) {
+        
+        totalAhorrado = totalAhorrado + aporteMensual;
+        double interesesGanados = totalAhorrado * tasaInteresMensual;        
+        totalAhorrado = totalAhorrado + interesesGanados;
+
+    }
+
+    std::cout << "\nResultado" << std::endl;
+    std::cout << "Despues de " << aniosTotales << " anios (o " << totalMeses << " meses)" << std::endl;
+    std::cout << "¡Habrias ahorrado un total de: $" << std::fixed << std::setprecision(2) << totalAhorrado << "!" << std::endl;
+    
+
+    return 0; 
 }
